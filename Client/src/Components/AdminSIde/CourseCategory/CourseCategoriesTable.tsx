187706c71045804cb60f26dd6c4fae3f5fa47194
SRUTHI-MURALI <srumurali2018@gmail.com>
2023-09-09 16:52:16 +0530

    update add category

--- Client/src/Components/AdminSIde/CourseCategory/CourseCategoriesTable.tsx
+++ Client/src/Components/AdminSIde/CourseCategory/CourseCategoriesTable.tsx
@@ -1,74 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 import Table from 'react-bootstrap/Table';
-import { ImArrowRight } from 'react-icons/im';
-import { Button, Col, Row } from 'react-bootstrap';
+import {ImArrowRight} from 'react-icons/im'
+import { Button, Col, Row } from 'react-bootstrap'
+import {useState,useEffect} from 'react'
 import axios from 'axios';
-import './CourseCategoriesTable.css';
+import './CourseCategoriesTable.css'
 import AddCategory from './AddCategory';
 
 function CourseCategoriesTable() {
-  const [categoryList, setCategorylist] = useState([]);
+
+  const[categoryList,setCategorylist]=useState([])
   const [openPopUp, setOpenPopUp] = useState(false);
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:3002/admin/getcategorylist')
-      .then((response) => {
-        setCategorylist(response.data.categories);
-        console.log(categoryList, 'category');
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-
-  const handleOpenDialog = () => {
-    setOpenPopUp(true);
-  };
-
+ 
+    
+    useEffect(() => {
+        axios.get('http://localhost:3002/admin/getcategorylist')
+          .then((response) => {
+           
+            setCategorylist(response.data.categories);
+            console.log(categoryList,"category");
+            
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }, []);
+  
+      const handleOpenDialog = () => {
+        setOpenPopUp(true);
+      
+        
+      };
+    
   return (
     <div>
       <Row>
+        <Row>
         <Col>
-          <p className='categorylistheading'>
-            <ImArrowRight /> <u>Course Categories</u>
-          </p>
+        <p className='categorylistheading' ><ImArrowRight /> <u>Course Categories</u></p>
         </Col>
         <Col>
-          <Button onClick={handleOpenDialog} type='submit' className='addcategorylist'>
-            Add Category
-          </Button>
+        <Button onClick={handleOpenDialog} type='submit' className='addcategorylist'>Add Category</Button>
+        </Col>
+        </Row>
+       
+      <Col>
+      {openPopUp==false &&(
+    <Table className='mt-5 ms-5' striped bordered hover size="sm">
+     <thead >
+       <tr>
+         <th>#</th>
+         <th > Title</th>
+         <th >Description</th>
+         
+       </tr>
+     </thead>
+     <tbody>
+         {categoryList.map((category, index) => (
+           <tr key={category._id}>
+             <td>{index + 1}</td>
+             <td>{category.title}</td>
+             <td>{category.description}</td>
+            
+           
+           </tr>
+         ))}
+       </tbody>
+   </Table>
+        )}
         </Col>
       </Row>
-      <Col>
-        {openPopUp == false && (
-          <Table className='mt-5 ms-5' striped bordered hover size='sm'>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Title</th>
-                <th>Description</th>
-              </tr>
-            </thead>
-            <tbody>
-              {categoryList.map((category, index) => (
-                <tr key={category._id}>
-                  <td>{index + 1}</td>
-                  <td>{category.title}</td>
-                  <td>{category.description}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        )}
-      </Col>
+
       {openPopUp && (
         <div>
-          <AddCategory onClose={() => setOpenPopUp(false)} />
+          <AddCategory onClose={()=> setOpenPopUp(false)}/>
+         
         </div>
       )}
-    </div>
-  );
+    
+   
+   
+     </div>
+    
+    
+  )
 }
 
-export default CourseCategoriesTable;
+export default  CourseCategoriesTable
+
